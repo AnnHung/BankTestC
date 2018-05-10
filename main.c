5d3fb9a7b3f2b96376600576f3e4fe1a4bc256df
yaolinh <nam.ngp@gmail.com>
2018-05-10 17:45:52 +0700

    begin to implement constrain

--- main.c
+++ main.c
@@ -23,6 +23,18 @@ const char DATA_FILE_PATH[] = ".\\data.dat";
 const long MAX_BANK_QUANTITY = 99999999;
 const int PAGINATION_NUMBER = 1000;
 const int BANK_NAME_LENGTH = 50;
+const int BANK_ADDRESS_LENGTH = 50;
+const int BANK_PHONE_LENGTH = 30;
+const int BANK_TYPE_LENGTH = 2;
+const int BANK_MEMBER_TYPE_LENGTH = 2;
+const int BANK_STATUS_LENGTH = 1;
+const char BANK_TYPE_HS[] = "HS";
+const char BANK_TYPE_CN[] = "CN";
+const char BANK_MEMBER_TYPE_NN[] = "NN";
+const char BANK_MEMBER_TYPE_CP[] = "CP";
+const char BANK_MEMBER_TYPE_KB[] = "KB";
+const char BANK_STATUS_TYPE_ACTIVE = 'Y';
+const char BANK_STATUS_TYPE_DEACTIVE = 'N';
 /**************/
 
 /*DECLARATION*/
@@ -31,8 +43,19 @@ struct CONFIGURATION{
 };
 struct BANK{
     long bankId;
-    char bankName[50];
+    char bankName[50];    
+    char bankAddress[100];
+    char phoneNo[30];
+    char bankType[2];
+    char memberType[2];
+    char status;
     int isDeleted;
+    struct BANK_DATE *foundDate;
+};
+struct BANK_DATE{
+    int day;
+    int month;
+    int year;
 };
 /*************/
 
@@ -131,11 +154,84 @@ void addNewBank(){
     fflush(stdin);
     struct BANK *newBank = malloc(sizeof(struct BANK));
     printf("ADD NEW BANK.\n");
-    printf("Please insert bank name: \n");   
+    /*Bank name*/
+    printf("Please insert bank name: \n"); 
     char *newBankName = malloc(sizeof(char[BANK_NAME_LENGTH]));
     fgets(newBankName,BANK_NAME_LENGTH,stdin);
-    strcpy(newBank->bankName,newBankName);  
+    strcpy(newBank->bankName,newBankName);
+    fflush(stdin);
+    /*Bank address*/
+    printf("Please insert bank address: \n"); 
+    char *newBankAddress = malloc(sizeof(char[BANK_ADDRESS_LENGTH]));
+    fgets(newBankName,BANK_ADDRESS_LENGTH,stdin);
+    strcpy(newBank->bankAddress,newBankAddress);
+    fflush(stdin);
+    /*Bank phone*/
+    printf("Please insert bank phone: \n"); 
+    char *newBankPhone = malloc(sizeof(char[BANK_ADDRESS_LENGTH]));
+    fgets(newBankName,BANK_ADDRESS_LENGTH,stdin);
+    strcpy(newBank->bankAddress,newBankAddress);
+    fflush(stdin);
+    /*Bank type*/
+    printf("Please insert bank type(CN/HS): \n"); 
+    printf("CN(Chi Nhanh) \n"); 
+    printf("HS(Hoi So) \n");
+    char *newBankType = malloc(sizeof(char[BANK_TYPE_LENGTH]));
+    fgets(newBankType,BANK_TYPE_LENGTH,stdin);
+    while(1==1){
+        fgets(newBankType,BANK_TYPE_LENGTH,stdin);
+        fflush(stdin);
+        if('\0' != newBankType[0] || 
+                0 != strcmp(BANK_TYPE_CN,newBankType) || 
+                0 != strcmp(BANK_TYPE_HS,newBankType)){
+            printf("Value must be 'CN' or 'HS'; Please re-insert: \n"); 
+        }
+    }
+    strcpy(newBank->bankType,newBankType);
+    /*Member type*/
+    printf("Please insert member type(NN/CP/KB): \n"); 
+    printf("NN(Ngan Hang Nha Nuoc) \n"); 
+    printf("CP(Ngan Hang TMCP) \n");
+    printf("KV(Kho Bac) \n");
+    char *newBankMemberType = malloc(sizeof(char[BANK_MEMBER_TYPE_LENGTH]));
+    fgets(newBankMemberType,BANK_TYPE_LENGTH,stdin);
+    while(1==1){
+        fgets(newBankMemberType,BANK_MEMBER_TYPE_LENGTH,stdin);
+        fflush(stdin);
+        if ('\0' != newBankType[0] 
+                || 0 != strcmp(BANK_MEMBER_TYPE_NN, newBankMemberType) 
+                || 0 != strcmp(BANK_MEMBER_TYPE_CP, newBankMemberType) 
+                || 0 != strcmp(BANK_MEMBER_TYPE_KB, newBankMemberType)) {
+            printf("Value must be 'NN' or 'CP' or 'KB'; Please re-insert: \n"); 
+        }
+    }
+    strcpy(newBank->memberType,newBankMemberType);
+    fflush(stdin);
+    /*Bank status*/
+    printf("Please insert bank status(Y/N): \n"); 
+    printf("Y(Dang Hoant Dong) \n"); 
+    printf("N(Da Dung Hoat Dong) \n");
+    char *newBankStatus = malloc(sizeof(char[BANK_STATUS_LENGTH]));
+    fgets(newBankStatus,BANK_TYPE_LENGTH,stdin);
+    while(1==1){
+        fgets(newBankStatus,BANK_TYPE_LENGTH,stdin);
+        fflush(stdin);
+        if('\0' != newBankStatus[0] || 
+                0 != strcmp(BANK_STATUS_TYPE_ACTIVE,newBankStatus) || 
+                0 != strcmp(BANK_STATUS_TYPE_DEACTIVE,newBankStatus)){
+            printf("Value must be 'Y' or 'N'; Please re-insert: \n"); 
+        }
+    }
+    strcpy(newBank->status,newBankStatus);
+    /*Bank found date*/
+    printf("Please enter bank found date: dd/mm/yyyy:\n");
+    int day, month, year;
+    scanf("%d/%d/%/d",day, month, year);
+    
+    
     newBank->isDeleted = 0;
+    
+    saveConfiguration(appConfig);
     fflush(stdin);
     saveNewBank(newBank);
 }
@@ -143,6 +239,7 @@ void addNewBank(){
 void saveNewBank(struct BANK *newBank){
     FILE *bankDataFile = fopen(DATA_FILE_PATH,"ab+");
     int newSequence = appConfig->currentSequence+=1;
+    saveConfiguration(appConfig);
     newBank->bankId = newSequence;
     fwrite(newBank,sizeof(struct BANK),1,bankDataFile);
     printf("SAVE NEW BANK SUCCESS.\n");
